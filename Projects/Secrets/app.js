b3295d7f58090ce52dfd06104ce4aa119caf0688
nanup <nanup.personal+github@gmail.com>
2022-07-27 14:00:20 +0530

    adds bcrypt hashing

--- Projects/Secrets/app.js
+++ Projects/Secrets/app.js
@@ -1,9 +1,12 @@
-require("dotenv").config();
-const express = require ("express")
-const bodyParser = require ("body-parser")
-const ejs = require ("ejs")
-const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const ejs = require ("ejs");
+const mongoose = require("mongoose");
+
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express()
 
@@ -20,37 +23,43 @@ const userSchema = new mongoose.Schema({
 	password: String
 });
 
-const secret = process.env.SECRET
-
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
-
 const secretUser = new mongoose.model("secretUser", userSchema)
 
 app.get("/", function(req, res) {
-	res.render("home")
+	res.render("home");
 });
 
 app.get("/register", function(req, res) {
-	res.render("register")
+	res.render("register");
 });
 
 app.get("/login", function(req, res) {
-	res.render("login")
+	res.render("login");
 });
 
 app.post("/register", function (req, res) {
-	const newUser = new secretUser({
-		email: req.body.username,
-		password: req.body.password
-	})
-
-	newUser.save(function(err) {
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
 		if (!err) {
-			res.render("secrets")
+			password = hash
 		} else {
-			console.log(err)
-		}
-	})
+			console.log(err);
+		};
+
+		const newUser = new secretUser({
+			email: req.body.username,
+			password: password
+			});
+	
+		newUser.save(function(err) {
+			if (!err) {
+				res.render("secrets")
+			} else {
+				console.log(err)
+			}
+		});
+	});
+
+	
 });
 
 app.post("/login", function(req, res) {
@@ -60,18 +69,23 @@ app.post("/login", function(req, res) {
 			if (err) {
 				console.log(err)
 			} else {
-				if (foundUser.password == req.body.password) {
-					res.render("secrets")
-				} 
+				const hash = foundUser.password
+				bcrypt.compare(req.body.password, hash, function(err, result) {
+					if (!err) {
+						res.render("secrets")
+					} else {
+						console.log(err)
+					}
+				})
 			}
 		}
-	)
-})
+	);
+});
 
 app.get("/logout", function(req, res) {
-	res.render("home")
+	res.render("home");
 });
 
 app.listen(3000, function() {
-	console.log("Server started on port 3000")
-})
+	console.log("Server started on port 3000");
+});
