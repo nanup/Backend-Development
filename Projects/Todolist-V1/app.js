d85217a4f9522282c9338b93a7344123955c9523
nanup <nanup.personal+github@gmail.com>
2023-08-26 09:52:52 +0530

    fix

--- Projects/Todolist-V1/app.js
+++ Projects/Todolist-V1/app.js
@@ -1,49 +1,49 @@
-const express = require ("express");
-const bodyParser = require ("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 let todate = new Date();
 let dateOptions = {
-    weekday: "long",
-    month: "long",
-    day: "numeric"
-}
-let date = todate.toLocaleDateString("en-US", dateOptions);
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+};
+let date = todate.toLocaleDateString('en-US', dateOptions);
 
 let dateEntryItems = [];
 let workEntryItems = [];
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get("/", function(req, res) {
-    res.render("list", {listTitle: date, entryItems: dateEntryItems})
-})
+app.get('/', function (req, res) {
+  res.render('list', { listTitle: date, entryItems: dateEntryItems });
+});
 
-app.post("/", function(req, res) {
-    let newEntryItem = req.body.newEntry;
+app.post('/', function (req, res) {
+  let newEntryItem = req.body.newEntry;
 
-    if (req.body.button == "Work List") {
-        workEntryItems.push(newEntryItem);
+  if (req.body.button == 'Work List') {
+    workEntryItems.push(newEntryItem);
 
-        res.redirect("/work");
-    } else {
-        dateEntryItems.push(newEntryItem);
+    res.redirect('/work');
+  } else {
+    dateEntryItems.push(newEntryItem);
 
-        res.redirect("/");
-    }
-})
+    res.redirect('/');
+  }
+});
 
-app.get("/work", function (req, res) {
-    res.render("list", {listTitle: "Work List", entryItems: workEntryItems})
-})
+app.get('/work', function (req, res) {
+  res.render('list', { listTitle: 'Work List', entryItems: workEntryItems });
+});
 
-app.get("/about", function(req, res) {
-    res.render("about");
-})
+app.get('/about', function (req, res) {
+  res.render('about');
+});
 
 app.listen(3000, function () {
-    console.log("Server is live on port 3000");
-})
+  console.log('Server is live on port 3000');
+});
